c240914422d3ad12de1276d4db354747213746b4
Magnus Rosén <mangez84@gmail.com>
2021-09-19 11:44:52 +0000

    Add shopping bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -17,7 +17,7 @@ def bag_contents(request):
             'item_id': item_id,
             'quantity': quantity,
             'product': product,
-        }) 
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
