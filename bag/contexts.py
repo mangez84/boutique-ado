08caabf36de1a67582c9ecd680e434414ba2e421
Magnus Rosén <mangez84@gmail.com>
2021-09-19 17:18:40 +0000

    Add quantity boxes to shopping bag page

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
+
 def bag_contents(request):
     bag_items = []
     total = 0
@@ -26,7 +27,7 @@ def bag_contents(request):
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'size': size,
                 })
